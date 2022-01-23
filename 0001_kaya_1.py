4f1ae7e9ada56eba59af4dd580af48338bc041d2
umurcankaya <umurcan.kaya@gmail.com>
2022-01-23 23:56:19 +0100

    pe1 umur

---
+++ 0001_kaya_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+from time import time
+ti = time()
+
+n = 100000
+
+result = np.sum(np.arange(0,n,3)) \
+	   + np.sum(np.arange(0,n,5)) \
+	   - np.sum(np.arange(0,n,15))\
+
+print(f'{result = }\nin {time()-ti :.3f} seconds')
